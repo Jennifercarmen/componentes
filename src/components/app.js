4f34b73dd713dd8427f6b98796891563614b095d
JenniferCarmen <jennifercarmens@gmail.com>
2018-03-14 09:26:25 -0500

    componentes de clase

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './Header';
 import Hobbys from './Hobbys';
-
-
+import Footer from './Footer';
 const App = () => (
 <div>
     <Header/>
     <Hobbys/>
+    <Footer/>
+
 
   </div>  
 )
